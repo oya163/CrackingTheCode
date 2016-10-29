c8db57b0fbd1e9ede19078e2c44f16d092f856ef
Oyashi CVPR <oyeshsin@hotmail.com>
2016-10-29 18:59:36 -0500

    Interview Question 1.1 solution using unordered set

--- CrackingTheCode/UniqueCharInString.cpp
+++ CrackingTheCode/UniqueCharInString.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <unordered_set>
 
 using namespace std;
 
 int main() {
-	cout << "I changed my code from my personal computer" << endl;
+	unordered_set<char> charSet;
+	bool uniqueString = true;
+	std::pair<unordered_set<char>::iterator, bool> retPair;
+	string inputString;
+	cout << "Please input the string: ";
+	cin >> inputString;
+	for (int i = 0; i < inputString.size(); i++) {
+		retPair = charSet.insert(inputString[i]);
+		if (retPair.second == false) {
+			uniqueString = false;
+			break;
+		}
+	}
+	if (uniqueString) {
+		cout << "Give input string has unique characters." << endl;
+	}
+	else {
+		cout << "Given input string has duplicate characters." << endl;
+	}
 }
