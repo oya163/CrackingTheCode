7fcad758743cd633c93779632ad77f6297eb0257
Oyashi CVPR <oyeshsin@hotmail.com>
2016-10-29 19:09:07 -0500

    Added comments and tidy up

--- CrackingTheCode/Chapter_1/UniqueCharInString.cpp
+++ CrackingTheCode/Chapter_1/UniqueCharInString.cpp
@@ -1,3 +1,9 @@
+/*
+	Implement an algorithm to determine if a string has all unique characters.
+	10/29/2016
+	Oyashi
+*/
+
 #include <iostream>
 #include <map>
 #include <string>
@@ -6,12 +12,17 @@
 using namespace std;
 
 int main() {
+	//Define required data structure
 	unordered_set<char> charSet;
 	bool uniqueString = true;
 	std::pair<unordered_set<char>::iterator, bool> retPair;
 	string inputString;
+
+	//Input string
 	cout << "Please input the string: ";
 	cin >> inputString;
+
+	//Check for the duplicate characters
 	for (int i = 0; i < inputString.size(); i++) {
 		retPair = charSet.insert(inputString[i]);
 		if (retPair.second == false) {
@@ -19,6 +30,8 @@ int main() {
 			break;
 		}
 	}
+
+	//Display the results accordingly
 	if (uniqueString) {
 		cout << "Give input string has unique characters." << endl;
 	}
