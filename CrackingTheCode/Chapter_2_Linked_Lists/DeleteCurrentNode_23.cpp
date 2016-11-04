cd920976c6671b0241e503541fb9c5e4128501a9
Oyashi CVPR <oyeshsin@hotmail.com>
2016-11-04 15:07:11 -0500

    Modified to include Node header file

--- CrackingTheCode/Chapter_2_Linked_Lists/DeleteCurrentNode_23.cpp
+++ CrackingTheCode/Chapter_2_Linked_Lists/DeleteCurrentNode_23.cpp
@@ -6,68 +6,13 @@
 */
 
 #include <iostream>
+#include "Node\LinkedNode.h"
 
 using namespace std;
 
-template <class T>
-struct Node {
-	T data;
-	Node<T>* next;
-
-	Node<T>() : data(), next() {}
-	Node<T>(T d) {
-		this->data = d;
-		this->next = NULL;
-	}
-};
-
-template <class T>
-class NodeClass {
-public:
-	void insert(Node<T>* &head, T val) {
-		Node<T>* newNode = new Node<T>(val);
-		if (head == NULL) {
-			head->data = val;
-			head->next = NULL;
-		}
-		else {
-			Node<T>* temp = head;
-			newNode->next = temp;
-			head = newNode;
-		}
-	}
-
-	void display(Node<T>* &head) {
-		if (head == NULL) {
-			cout << "Empty list" << endl;
-		}
-		else {
-			Node<T>* curr = head;
-			while (curr) {
-				cout << curr->data << " ";
-				curr = curr->next;
-			}
-		}
-		cout << endl;
-	}
-
-	bool removeNode(Node<T>* &node) {
-		if (node == NULL || node->next == NULL) {
-			return false;
-		}
-		else {
-			Node<T>* newNode = node->next;
-			node->data = newNode->data;
-			node->next = newNode->next;
-			return true;
-		}
-	}
-
-};
-
 int main() {
 	int input = 1;
-	NodeClass<int> node;
+	LinkedNode<int> node;
 	Node<int>* newNode = new Node<int>();
 	while (input > 0) {
 		cin >> input;
