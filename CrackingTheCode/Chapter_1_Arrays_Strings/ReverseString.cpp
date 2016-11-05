2a3ea2d93c6624a85714ff8c21e8d5ff3508d548
Oyashi CVPR <oyeshsin@hotmail.com>
2016-11-05 13:33:07 -0500

    Added from lab PC

--- CrackingTheCode/Chapter_1_Arrays_Strings/ReverseString.cpp
+++ CrackingTheCode/Chapter_1_Arrays_Strings/ReverseString.cpp
@@ -1,35 +1,35 @@
-///*
-//	Implement a function void reverse(char* str) in C or C++ which reverses a nullterminated string.
-//	Solution from the book
-//	10/29/2016
-//	Oyashi
-//*/
-//
-//#include <iostream>
-// 
-//using namespace std;
-//
-//void reverse(char *str) {
-//	char* end = str;
-//	char tmp;
-//	if (str) {
-//		while (*end) {
-//			++end;
-//		}
-//		--end;
-//
-//		while (str < end) {
-//			tmp = *str;
-//			*str++ = *end;
-//			*end-- = tmp;
-//		}
-//	}
-//}
-//
-//int main() {
-//	
-//	char inputString[] = "oyesh";
-//	reverse(inputString);
-//	cout << inputString << endl;
-//
-//}
+/*
+	Implement a function void reverse(char* str) in C or C++ which reverses a nullterminated string.
+	Solution from the book
+	10/29/2016
+	Oyashi
+*/
+
+#include <iostream>
+ 
+using namespace std;
+
+void reverse(char *str) {
+	char* end = str;
+	char tmp;
+	if (str) {
+		while (*end) {
+			++end;
+		}
+		--end;
+
+		while (str < end) {
+			tmp = *str;
+			*str++ = *end;
+			*end-- = tmp;
+		}
+	}
+}
+
+int main() {
+	
+	char inputString[] = "oyesh";
+	reverse(inputString);
+	cout << inputString << endl;
+
+}
--- CrackingTheCode/Chapter_1_Arrays_Strings/UniqueCharInString.cpp
+++ CrackingTheCode/Chapter_1_Arrays_Strings/UniqueCharInString.cpp
@@ -1,41 +1,41 @@
-///*
-//	Implement an algorithm to determine if a string has all unique characters.
-//	10/29/2016
-//	Oyashi
-//*/
-//
-//#include <iostream>
-//#include <map>
-//#include <string>
-//#include <unordered_set>
-//
-//using namespace std;
-//
-//int main() {
-//	//Define required data structure
-//	unordered_set<char> charSet;
-//	bool uniqueString = true;
-//	std::pair<unordered_set<char>::iterator, bool> retPair;
-//	string inputString;
-//
-//	//Input string
-//	cout << "Please input the string: ";
-//	cin >> inputString;
-//
-//	//Check for the duplicate characters
-//	for (int i = 0; i < inputString.size(); i++) {
-//		retPair = charSet.insert(inputString[i]);
-//		if (retPair.second == false) {
-//			uniqueString = false;
-//			break;
-//		}
-//	}
-//
-//	//Display the results accordingly
-//	if (uniqueString) {
-//		cout << "Give input string has unique characters." << endl;
-//	}
-//	else {
-//		cout << "Given input string has duplicate characters." << endl;
-//	}
-//}
+/*
+	Implement an algorithm to determine if a string has all unique characters.
+	10/29/2016
+	Oyashi
+*/
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+	//Define required data structure
+	unordered_set<char> charSet;
+	bool uniqueString = true;
+	std::pair<unordered_set<char>::iterator, bool> retPair;
+	string inputString;
+
+	//Input string
+	cout << "Please input the string: ";
+	cin >> inputString;
+
+	//Check for the duplicate characters
+	for (int i = 0; i < inputString.size(); i++) {
+		retPair = charSet.insert(inputString[i]);
+		if (retPair.second == false) {
+			uniqueString = false;
+			break;
+		}
+	}
+
+	//Display the results accordingly
+	if (uniqueString) {
+		cout << "Give input string has unique characters." << endl;
+	}
+	else {
+		cout << "Given input string has duplicate characters." << endl;
+	}
+}
