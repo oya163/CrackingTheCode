836f9d256582e34769f5614e950bd808f9b5bd6b
oya163 <oyeshsin@hotmail.com>
2017-02-20 22:00:19 -0600

    Removed the comments

--- CrackingTheCode/Chapter_3_Stacks_Queues/Queue_Implementation.cpp
+++ CrackingTheCode/Chapter_3_Stacks_Queues/Queue_Implementation.cpp
@@ -1,22 +1,22 @@
-///*
-//	Generic Queue Implementation
-//	Oyashi
-//	12/22/2016
-//*/
-//
-//#include <iostream>
-//#include "GenericQueue.h"
-//
-//using namespace std;
-//
-//int main() {
-//	Queue<int> queue;
-//	queue.enqueue(5);
-//	queue.enqueue(6);
-//	queue.enqueue(7);
-//	queue.display();
-//
-//	queue.dequeue();
-//	queue.display();
-//	queue.~Queue();
-//}
+/*
+	Generic Queue Implementation
+	Oyashi
+	12/22/2016
+*/
+
+#include <iostream>
+#include "GenericQueue.h"
+
+using namespace std;
+
+int main() {
+	Queue<int> queue;
+	queue.enqueue(5);
+	queue.enqueue(6);
+	queue.enqueue(7);
+	queue.display();
+
+	queue.dequeue();
+	queue.display();
+	queue.~Queue();
+}
